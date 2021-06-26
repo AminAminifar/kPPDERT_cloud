869a0c734ea6f370de2edb1db8ab08d864b2ede5
AminAminifar <AminAminifar@users.noreply.github.com>
2021-06-26 18:32:10 +0200

    Initialization phase

--- Initialization/main.py
+++ Initialization/main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
+import csv
+from Cryptodome.PublicKey import RSA
+from Cryptodome import Random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import math
+import random
+from Cryptodome.Cipher import PKCS1_OAEP
+
+print("hi----------------------------------------------------")
+
+key = RSA.generate(2048)
+publickey = key.publickey()
+
+# seed = 100
+#
+# en_seed = publickey.encrypt(long(seed), '')[0]
+#
+# print("en_seed: ", en_seed)
+# de_seed = key.decrypt(en_seed)
+# print("de_seed: ", de_seed)
+#
+#
+# seed2 = 5
+# en_seed2 = publickey.encrypt(long(seed2), '')[0]
+# mul = en_seed * en_seed2
+# print("mul: ", mul)
+# de_mul = key.decrypt(mul)
+# print("de_mul: ", de_mul)
+
+cipher = PKCS1_OAEP.new(key)
+text = b'The secret I want to send.'
+en_text = cipher.encrypt(text)
+print("en_text: ", en_text)
+# de_text = cipher.encrypt(en_text)
+# print("de_text: ", de_text)
+
+keyPair = RSA.generate(3072)
+pubKey = keyPair.publickey()
+pubKeyPEM = pubKey.exportKey()
+# print(pubKeyPEM.decode('ascii'))
+privKeyPEM = keyPair.exportKey()
+# print(privKeyPEM.decode('ascii'))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+msg = b'100'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:",encrypted)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+print('Decrypted:',decrypted)
+print('num:',int(decrypted))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# en_text = pubKey.encrypt('hhh')
+# print("en_text: ", en_text)
+# de_text = privKeyPEM.encrypt(en_text)
+# print("de_text: ", de_text)
+
+
+# f = open('secret.txt','wb')
+# f.write(key.export_key('PEM'))
+# f.close()
+# ...
+# >>> f = open('mykey.pem','r')
+# >>> key = RSA.import_key(f.read())
